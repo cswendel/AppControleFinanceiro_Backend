e55e9f8a26d67a5ec816e4ab6d4d985c66147084
Wendel Conceição Souza <wendelconceicaosouza@gmail.com>
2025-04-20 20:11:38 -0300

    Implementado o BD com o JPA e atualizado a transacao de acordo com o BD

--- src/main/java/br/ueg/ProgWeb1/AppControleFinanceiro/model/Transacao.java
+++ src/main/java/br/ueg/ProgWeb1/AppControleFinanceiro/model/Transacao.java
@@ -1,5 +1,6 @@
 package br.ueg.ProgWeb1.AppControleFinanceiro.model;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -10,12 +11,28 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Entity
 public class Transacao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long Id;
+
+    @Column(nullable = false)
     private Double Valor;
+
+    @Column(nullable = false)
     private boolean Tipo; //Entrada=1 Saída=0
+
+    @Column(nullable = false)
     private String Descricao;
+
+    @Column(nullable = false)
     private String Categoria;
+
+    @Column(nullable = false)
     private LocalDate Data;
+
+    @Column(nullable = false)
     private String Status;
 }
