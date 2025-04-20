536377ab4fe04b185d526a626a0d73cf6ff56aa8
Wendel Conceição Souza <wendelconceicaosouza@gmail.com>
2025-04-20 19:45:04 -0300

    Estrutura base do Java/Spring

---
+++ src/test/java/br/ueg/ProgWeb1/AppControleFinanceiro/AppControleFinanceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ueg.ProgWeb1.AppControleFinanceiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppControleFinanceiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
