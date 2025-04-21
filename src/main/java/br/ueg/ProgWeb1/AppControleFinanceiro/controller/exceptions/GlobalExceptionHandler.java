0f85f15125f972007c43de1e9092bec4b2106ff4
Wendel Conceição Souza <wendelconceicaosouza@gmail.com>
2025-04-21 17:03:41 -0300

    Criação do package exception dentro do controller para melhor exibição

---
+++ src/main/java/br/ueg/ProgWeb1/AppControleFinanceiro/controller/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,39 @@
+package br.ueg.ProgWeb1.AppControleFinanceiro.controller.exceptions;
+
+import br.ueg.ProgWeb1.AppControleFinanceiro.service.exceptions.BusinessException;
+import io.swagger.v3.oas.annotations.Hidden;
+import lombok.Data;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.lang.reflect.InvocationTargetException;
+
+@Hidden
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BusinessException.class)
+    public ResponseEntity<ErrorResponse> handleBusinessException(BusinessException ex) {
+        ErrorResponse error = new ErrorResponse(ex.getCodeError(), ex.getMessage());
+        return new ResponseEntity<>(error, HttpStatusCode.valueOf(ex.getCodeError()));
+    }
+
+    @Data
+    public static class ErrorResponse {
+        private int code;
+        private String message;
+
+        public ErrorResponse(int code, String message) {
+            this.code = code;
+            this.message = message;
+        }
+    }
+
+    @ExceptionHandler({ InvocationTargetException.class, RuntimeException.class, Exception.class })
+    public ResponseEntity<Object> handleException(final Throwable e) {
+        ErrorResponse error = new ErrorResponse(500, e.getMessage());
+        return new ResponseEntity<>(error, HttpStatusCode.valueOf(500));
+    }
+}
